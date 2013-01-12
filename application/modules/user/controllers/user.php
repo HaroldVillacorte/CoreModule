87245bcd652e71060519979862d5304a728a1d49
HaroldVillacorte <admin@laughinghost.com>
2013-01-12 06:49:50 -0700

    Cleanup.

--- application/modules/user/controllers/user.php
+++ application/modules/user/controllers/user.php
@@ -40,64 +40,64 @@ class User extends MX_Controller {
   	  $user_role = '';
   	}
   	if ($role != $user_role) {  		
-  		if ($this->session->userdata('user_id')) {
-  			$message = 'That page requires ' . $role . ' permission. Please login as admin.';
-  			$this->session->set_flashdata('message_error', $message);
+      if ($this->session->userdata('user_id')) {
+        $message = 'That page requires ' . $role . ' permission. Please login as admin.';
+        $this->session->set_flashdata('message_error', $message);
   		  redirect(base_url() . 'user/profile/' . $this->session->userdata('user_id'));
-  		}
-  		else {
-  			$message = 'That page requires ' . $role . ' permission. Please login.';
-  			$this->session->set_flashdata('message_error', $message);
-  			redirect(base_url() . 'user/login');
-  		}
-  	}
+      }
+      else {
+        $message = 'That page requires ' . $role . ' permission. Please login.';
+        $this->session->set_flashdata('message_error', $message);
+        redirect(base_url() . 'user/login');
+      }
+    }
   }
 
   public function login()
   {
-  	if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {
   	  $rules = array(
   	    array(
   	      'field' => 'username',
-  	    	'label' => 'Username',
-  	    	'rules' => 'required',
-  	    ),
-  	  	array(
-  	  		'field' => 'password',
-  	  		'label' => 'Password',
-  	  		'rules' => 'required',
-  	  	),
-  	  );
+          'label' => 'Username',
+          'rules' => 'required',
+        ),
+        array(
+          'field' => 'password',
+          'label' => 'Password',
+          'rules' => 'required',
+        ),
+      );
   	  $this->form_validation->set_rules($rules);
   	  if ($this->form_validation->run() == FALSE) {
-  	  	// Form does not validate.
-  	    self::$data['view_file'] = 'user_login';
-  	    echo Modules::run('template/default_template', self::$data);
-  	  }
+        // Form does not validate.
+        self::$data['view_file'] = 'user_login';
+        echo Modules::run('template/default_template', self::$data);
+      }
   	  else {
   	    // Encrypt password and send to database.
-  	  	$_POST['password'] = sha1($_POST['password']);
-  	    if ($this->user_model->login($_POST)) {
-  	    	// Login success.
-  	    	$this->session->set_flashdata('message_success', 'You are now logged in as ' . $_POST['username'] . '.');
-  	      redirect(base_url());
-  	    }
+        $_POST['password'] = sha1($_POST['password']);
+        if ($this->user_model->login($_POST)) {
+          // Login success.
+          $this->session->set_flashdata('message_success', 'You are now logged in as ' . $_POST['username'] . '.');
+          redirect(base_url());
+        }
   	    else {
-  	    	// Unseccessful login.
-  	    	$this->session->set_flashdata('message_error', 'Username and password combination not found.');
-  	    	redirect(current_url());
-  	    }
-  	  }
-  	}
+          // Unseccessful login.
+          $this->session->set_flashdata('message_error', 'Username and password combination not found.');
+          redirect(current_url());
+        }
+      }
+    }
     self::$data['view_file'] = 'user_login';
     echo Modules::run('template/default_template', self::$data);
   }
-  
+
   public function logout() {
-  	$userarray = array('user_id', 'username', 'password', 'email', 'first_name', 'last_name', 'role',);
-  	$this->session->unset_userdata($userarray);
-  	$this->session->sess_destroy();
-  	redirect(base_url());
+    $userarray = array('user_id', 'username', 'password', 'email', 'first_name', 'last_name', 'role',);
+    $this->session->unset_userdata($userarray);
+    $this->session->sess_destroy();
+    redirect(base_url());
   }
 
   public function crud()
@@ -191,19 +191,19 @@ class User extends MX_Controller {
         $_POST['role'] = 'authenticated';
         // See user_model.php.  Save user method returns $id as opposed to boolean.
         if ($result = $this->user_model->save_user($_POST)) {
-        	if ($result == 'updated') {
-        	  $this->session->set_flashdata('message_success', $_POST['username'] . ' saved succesfully.');
+          if ($result == 'updated') {
+            $this->session->set_flashdata('message_success', $_POST['username'] . ' saved succesfully.');
             redirect(current_url());
-        	}
-        	elseif ($result == 'inserted') {
-        		$this->session->set_flashdata('message_success', $_POST['username'] . ' saved succesfully.');
-        		redirect(base_url() . 'user/login');
-        	}
-        	elseif ($result_id = $this->user_model->save_user($_POST) == 'protected') {
-        		$this->session->set_flashdata('message_error', 'Unable to save.  Account is protected.');
-        		redirect(current_url());
-        	}
-        	else {
+          }
+          elseif ($result == 'inserted') {
+            $this->session->set_flashdata('message_success', $_POST['username'] . ' saved succesfully.');
+            redirect(base_url() . 'user/login');
+          }
+          elseif ($result_id = $this->user_model->save_user($_POST) == 'protected') {
+            $this->session->set_flashdata('message_error', 'Unable to save.  Account is protected.');
+        	  redirect(current_url());
+          }
+          else {
             $this->session->set_flashdata('message_error', 'Unable to save user. Please contact administrator.');
             redirect(current_url());
           }
@@ -221,7 +221,7 @@ class User extends MX_Controller {
       echo Modules::run($this->template, self::$data);
     }
   }
-	
+
   public function delete($id = NULL) {
     if (isset($_POST['delete'])) {
       if ($this->user_model->delete_user($_POST['id']) == 'success') {
@@ -229,8 +229,8 @@ class User extends MX_Controller {
         redirect(base_url() . 'user/logout');
       }
       elseif ($this->user_model->delete_user($_POST['id']) == 'protected') {
-      	$this->session->set_flashdata('message_error', 'Unable to delete.  Account is protected.');
-      	redirect(base_url() . 'user/crud') ;
+        $this->session->set_flashdata('message_error', 'Unable to delete.  Account is protected.');
+        redirect(base_url() . 'user/crud') ;
       }
       else {
         $this->session->set_flashdata('message_error', 'Unable to delete your account.  Please contact administrator.');
